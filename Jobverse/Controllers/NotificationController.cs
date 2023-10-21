53205e5c4666b8a29dc2e05b93c182a530fe6522
Abdullah <122647095+Abdullah5111@users.noreply.github.com>
2023-10-21 23:25:57 +0500

    Notifications

---
+++ Jobverse/Controllers/NotificationController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Jobverse.Controllers;
+
+namespace Jobverse.Controllers
+{
+    public class TestNotification
+    {
+        public string UserEmail { get; set; }
+        public string Applier { get; set; }
+        public string Title { get; set; }
+        public DateTime Time { get; set; }
+        public bool Status { get; set; }
+    }
+
+    public class NotificationController : Controller
+    {
+        public IActionResult Notification()
+        {
+            List<TestNotification> testNotifications = new List<TestNotification>
+        {
+            new TestNotification
+            {
+                UserEmail = "user1@example.com",
+                Applier = "John Doe",
+                Title = "Senior SQ Engineer",
+                Time = DateTime.Now,
+                Status = true
+            },
+            new TestNotification
+            {
+                UserEmail = "user2@example.com",
+                Applier = "Jane Smith",
+                Title = "Junior Graphic Designer",
+                Time = DateTime.Now,
+                Status = true
+            },
+            new TestNotification
+            {
+                UserEmail = "user1@example.com",
+                Applier = "John Doe",
+                Title = "Senior SQ Engineer",
+                Time = DateTime.Now,
+                Status = false
+            },
+            new TestNotification
+            {
+                UserEmail = "user2@example.com",
+                Applier = "Jane Smith",
+                Title = "Junior Graphic Designer",
+                Time = DateTime.Now,
+                Status = true
+            },
+        };
+            return View(testNotifications);
+        }
+    }
+
+}
