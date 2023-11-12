932f38cf3b736365c8d3128f0111af6961b53705
Abdullah <122647095+Abdullah5111@users.noreply.github.com>
2023-11-12 13:30:26 +0500

    Notifications Microservice

---
+++ Jobverse/Jobverse/Controllers/NotificationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Jobverse.Models;
+
+namespace Jobverse.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        public NotificationController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:44379/");
+        }
+
+        public async Task<IActionResult> Notification()
+        {
+            var response = await _httpClient.GetAsync("api/notification");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var notifications = JsonConvert.DeserializeObject<List<Notification>>(content);
+
+                return View(notifications);
+            }
+            else
+            {
+                // Handle the case where the API request was not successful
+                return View("Error");
+            }
+        }
+    }
+
+}
